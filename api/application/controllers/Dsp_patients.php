e058e27fe5d69551de00c692e57fc5013f60d7a6
elex <elextraza@gmail.com>
2016-12-28 12:46:20 +0400

    Фильтр

--- api/application/controllers/Dsp_patients.php
+++ api/application/controllers/Dsp_patients.php
@@ -63,13 +63,24 @@ class Dsp_patients extends CI_Controller {
                 if(!isset($data['limit']))  $data['limit'] = 100;
                 if(!isset($data['offset']))  $data['offset'] = 0;
 
+                $arg['age_beg'] = 21;
+                $arg['age_end'] = 99;
+                if(isset($patient['age_beg'])) $arg['age_beg'] = $patient['age_beg'];
+                if(isset($patient['age_end'])) $arg['age_end']= $patient['age_end'];
+
+
+                $arg['month_beg'] = 1;
+                $arg['month_end'] = 12;
+                if(isset($patient['month_beg'])) $arg['month_beg'] = $patient['month_beg'];
+                if(isset($patient['month_end'])) $arg['month_end']= $patient['month_end'];
+
                 /*только 400 записей максимум*/
                 if((int)$data['limit']>400) $data['limit'] = 400;
                 if((int)$data['offset']<0) $data['offset'] = 0;
 
                 $res['patients']['rows'] = $this->patient_model->GetPatients($arg,$data['limit'],$data['offset']);
-                if(count($res['patients']['rows'])>0)
-                    $res['patients']['total'] = $res['patients']['rows'][0]['total'];
+                /*if(count($res['patients']['rows'])>0)
+                    $res['patients']['total'] = $this->patient_model->GetPatientsTotal($arg,$data['limit'],$data['offset']);;*/
             }
 
         } else {
@@ -79,6 +90,19 @@ class Dsp_patients extends CI_Controller {
         echo json_encode($res);
     }
 
+    public function GetRegLPUDoctors(){
+        $res = array();
+        if($this->auth_model->IsLogin()) {
+            $res['auth'] = 1;
+            $res['user'] = $this->auth_model->UserInfo();
+            $res['doctors'] = $this->patient_model->GetLPUDoctors($res['user']['lpucode']);
+
+        } else {
+            $res['auth'] = 0;
+        }
+
+        echo json_encode($res);
+    }
 
 
 
--- api/application/models/Patient_model.php
+++ api/application/models/Patient_model.php
@@ -27,34 +27,125 @@ class Patient_model extends CI_Model
 
         $limit = (int)$limit;
         $offset = (int)$offset;
-
+        $lpu = (int)$arg['lpucode'];
 
         /*сортировка*/
         $order_by=' order by surname ';
         if(isset($arg['sort']))
         $order_by = ' order by '.$arg['sort']." ".$arg['order'];
 
-        $sql="select *
+        $sql="
+declare @month_beg int = ".$arg['month_beg'].";
+declare @month_end int = ".$arg['month_end'].";
+declare @age_beg int = ".$arg['age_beg'].";
+declare @age_end int = ".$arg['age_end'].";
+declare @drcode varchar(8);
+declare @uch int = 1;
+
+declare @year int = 3;
+declare @lpu int = ".$lpu.";
+select *
 from
-(select ROW_NUMBER() over(".$order_by.") as rn, *,
+(select year(getdate()) - year(i.BIRTHDAY) as vozr,
+ ROW_NUMBER() over(order by i.surname) as rn,
+ null as user_id,
 
-(select count(*) from oms..OMSC_INSURED
-where d_fin is null and lpuchief = ".$arg['lpucode'].") as total
+ 2017 as  disp_year,
+ 1 as disp_type,
+ i.lpuchief as disp_lpu,
+ year(getdate()) - year(i.BIRTHDAY) as  age,
 
-from oms..OMSC_INSURED
-where (d_fin is null) and (lpuchief = ".$arg['lpucode'].")
+ i.lpubase,
+ i.lpubase_u,
+ i.type_u as typeui,
 
+ i. enp,
+
+(select count(*) from oms..OMSC_INSURED_SREZ
+where d_fin is null
+  and lpuchief = @lpu
+  and (year(getdate()) - year(i.BIRTHDAY)) % 3 = 0
+  and year(getdate()) - year(i.BIRTHDAY) >=21
+  and i.LPUBASE_U = @uch
+  ) as kol,
+
+
+  (select top 1 drcode
+      from aktpak..akpc_tmodoc
+	  where d_fin is null
+
+		and DBSOURCE = 'D'
+
+		and isnull(LPUTER_U,0) = isnull(i.lpubase_u,0)
+		and isnull(LPUTER,0) = isnull(i.lpubase,0)
+		and isnull(type_u,0) = isnull(i.type_u,0)
+      order by drcode
+      )  as drcode,
+  (select top 1 speccode
+      from aktpak..akpc_tmodoc
+	  where d_fin is null
+
+		and DBSOURCE = 'D'
+
+		and isnull(LPUTER_U,0) = isnull(i.lpubase_u,0)
+		and isnull(LPUTER,0) = isnull(i.lpubase,0)
+		and isnull(type_u,0) = isnull(i.type_u,0)
+      order by drcode
+      )  as speccode,
+
+  i.surname as surname1, i.name as name1, i.secname as secname1, i.birthday as birthday1
+
+  ,dp.[status],
+  pld.NAME
+from oms..OMSC_INSURED_SREZ i
+
+
+left join [DISP_WEB]..disp_plan dp
+on dp.enp=i.ENP
+
+left join [POLYCLINIC_2010].[dbo].[POLM_LPU_DISTRICTS] pld
+on (pld.NUM = i.LPUBASE_U)and(pld.LPUCODE = i.LPUBASE)and(pld.D_FIN is null)
+
+
+
+where i.d_fin is null
+  and i.lpuchief = @lpu
+  and (year(getdate()) - year(i.BIRTHDAY)) % 3 = 0
+  and year(getdate()) - year(i.BIRTHDAY) >= 21
+  and month(i.birthday) between @month_beg and @month_end
+  and (year(getdate()) - year(i.BIRTHDAY)) between  @age_beg and @age_end
+
+ and i.LPUBASE_U = @uch
+
+	-- having drcode = @drcode
 ) x
-where rn between ".$offset." and ".($offset+$limit)."
-";
-
+where rn between ".$offset." and ".$limit;
 
         $query = $this->db_mssql->conn_id->query($sql);
         /*http://proft.me/2008/11/28/primery-ispolzovaniya-pdo/*/
 
 
         return $this->elex->result_array($query);
+    }
 
+    /*выдает докторов в лпу*/
+    function GetLPUDoctors($lpucode) {
+        $lpucode = (int)$lpucode;
+        $sql = "
+    select * from [AKTPAK].[dbo].[AKPC_DOCTORS] d
+    where (d.LPUWORK = ".$lpucode.")AND(d.D_FIN is null)and(d.DBSOURCE = 'D') order by d.SURNAME;";
+        $query = $this->db_mssql->conn_id->query($sql);
+        return $this->elex->result_array($query);
+    }
+
+    /*выдает участки в лпу*/
+    function GetLPUuch($lpucode) {
+        $lpucode = (int)$lpucode;
+        $sql = "
+    select * from [AKTPAK].[dbo].[AKPC_DOCTORS] d
+    where (d.LPUWORK = ".$lpucode.")AND(d.D_FIN is null)and(d.DBSOURCE = 'D') order by d.SURNAME;";
+        $query = $this->db_mssql->conn_id->query($sql);
+        return $this->elex->result_array($query);
     }
 
 }
